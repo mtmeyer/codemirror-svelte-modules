8e4f58828b30c308593a8df774f2da5f29b41b61
Michael Meyer <9434150+mtmeyer@users.noreply.github.com>
2022-05-09 22:27:10 +1000

    feat: added basic codemirror component

---
+++ src/lib/CodeMirror.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	import { onMount, createEventDispatcher } from 'svelte';
+	import { EditorState } from '@codemirror/state';
+	import type { Extension } from '@codemirror/state';
+	import { EditorView } from '@codemirror/view';
+	import { oneDark } from '@codemirror/theme-one-dark';
+	import { basicSetup as basicSetupExtension } from '@codemirror/basic-setup';
+
+	export let extensions: Extension[] = [];
+	export let height = '';
+	export let minHeight = '';
+	export let maxHeight = '';
+	export let width = '';
+	export let minWidth = '';
+	export let maxWidth = '';
+	export let editable = true;
+	export let readOnly = false;
+	export let autoFocus = false;
+	export let theme = oneDark;
+	export let basicSetup = false;
+	export let initialValue = 'Hello world';
+
+	let editorElement: HTMLDivElement;
+
+	const dispatch = createEventDispatcher();
+
+	let view;
+	let state;
+
+	const editableExtension = EditorView.editable.of(editable);
+	const readOnlyExtension = EditorState.readOnly.of(readOnly);
+
+	let updateListenerExtension = EditorView.updateListener.of((update) => {
+		if (update.docChanged) {
+			dispatch('change', update);
+		}
+	});
+
+	const initialExtensions = () => {
+		let defaultExtensions = [
+			defaultThemeOption,
+			updateListenerExtension,
+			editableExtension,
+			readOnlyExtension,
+			theme
+		];
+		if (basicSetup) defaultExtensions.push(basicSetupExtension);
+
+		return defaultExtensions;
+	};
+
+	const defaultThemeOption = EditorView.theme({
+		'&': {
+			height,
+			minHeight,
+			maxHeight,
+			width,
+			minWidth,
+			maxWidth
+		}
+	});
+
+	onMount(async () => {
+		state = EditorState.create({
+			doc: initialValue,
+			extensions: [...initialExtensions(), ...extensions]
+		});
+
+		view = new EditorView({
+			state,
+			parent: editorElement
+		});
+
+		if (autoFocus) view.focus();
+	});
+</script>
+
+<div bind:this={editorElement} style="height: 200px;" />
+
+<style></style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import CodeMirror from '../lib/CodeMirror.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<CodeMirror basicSetup />
