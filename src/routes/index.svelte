4c89e2c55787aa8b59370b298dc79daf46af970a
Michael Meyer <9434150+mtmeyer@users.noreply.github.com>
2022-05-14 13:46:59 +1000

    feat: add placeholder attribute

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import CodeMirror from '../lib/CodeMirror.svelte';
+	let value = '';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<CodeMirror basicSetup initialValue="# Hello world" />
+<CodeMirror basicSetup placeholder="A placeholder" />
